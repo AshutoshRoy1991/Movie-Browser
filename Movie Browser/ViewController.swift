7def86c14177f83c6793fae65a168b331760109f
Ashutosh Roy <royashutosh1991@gmail.com>
2020-07-16 22:52:14 +0530

    Initial Commit

---
+++ Movie Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Browser
+//
+//  Created by Ashutosh Roy on 16/07/20.
+//  Copyright © 2020 Ashutosh Roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
